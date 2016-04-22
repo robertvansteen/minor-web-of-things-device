443b77510886d5af60bcf0a4ac468cde92966c68
reauv <studio@rovansteen.nl>
2016-04-22 12:49:02 +0200

    Filter misreadings from temp sensor

--- src/InputDevices/Temp.cpp
+++ src/InputDevices/Temp.cpp
@@ -16,6 +16,11 @@ boolean Temp::loop() {
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    return false;
+  }
+
   float hic = dht.computeHeatIndex(t, h, false);
 
   StaticJsonBuffer<1024> jsonBuffer;
