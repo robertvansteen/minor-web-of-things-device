7e4470610564ab13be0f00fddce74bdc7c3760c4
reauv <studio@rovansteen.nl>
2016-04-22 11:51:22 +0200

    Update example env

---
+++ src/env.example.cpp
@@ -0,0 +1,11 @@
+#include "env.h"
+
+const String ENV_DEVICE_NAME = "nodemcu_robert";
+const String ENV_DEVICE_LABEL = "NodeMCU Robert";
+const char* ENV_SSID = "";
+const char* ENV_PASSWORD = "";
+
+const char* ENV_SERVER_ADDRESS = "http://iot.rovansteen.nl";
+const char* ENV_PUBSUB_HOST = "http://iot.rovansteen.nl";
+const int ENV_SERVER_PORT = 3000;
+const int ENV_PUBSUB_PORT = 1883;
